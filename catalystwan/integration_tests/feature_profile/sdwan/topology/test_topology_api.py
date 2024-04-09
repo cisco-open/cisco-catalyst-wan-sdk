09ea42685c9fcda0672fee99c2b9664066ed73d9
sbasan <sbasan@cisco.com>
2024-04-09 15:03:15 +0200

    update test

--- catalystwan/integration_tests/feature_profile/sdwan/topology/test_topology_api.py
+++ catalystwan/integration_tests/feature_profile/sdwan/topology/test_topology_api.py
@@ -3,16 +3,22 @@ import unittest
 from typing import cast
 from uuid import UUID
 
-from catalystwan.api.feature_profile_api import TopologyFeatureProfileAPI
+from catalystwan.api.configuration_groups.parcel import as_global
+from catalystwan.api.feature_profile_api import ServiceFeatureProfileAPI, TopologyFeatureProfileAPI
+from catalystwan.models.configuration.feature_profile.sdwan.service.lan.vpn import LanVpnParcel
 from catalystwan.models.configuration.feature_profile.sdwan.topology.hubspoke import HubSpokeParcel
 from catalystwan.models.configuration.feature_profile.sdwan.topology.mesh import MeshParcel
 from catalystwan.session import ManagerSession, create_manager_session
 
 
-class TestTopologyFeatureProfileModels(unittest.TestCase):
+class TestTopologyFeatureProfile(unittest.TestCase):
     session: ManagerSession
-    api: TopologyFeatureProfileAPI
-    profile_id: UUID
+    topology_api: TopologyFeatureProfileAPI
+    topology_profile_id: UUID
+    service_api: ServiceFeatureProfileAPI
+    service_profile_id: UUID
+    lanvpn_parcel_name: str
+    lanvpn_parcel_id: UUID
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -22,16 +28,26 @@ class TestTopologyFeatureProfileModels(unittest.TestCase):
             username=cast(str, os.environ.get("TEST_VMANAGE_USERNAME")),
             password=cast(str, os.environ.get("TEST_VMANAGE_PASSWORD")),
         )
-        cls.api = cls.session.api.sdwan_feature_profiles.topology
-        cls.profile_id = cls.api.create_profile("TestProfile", "Description").id
+        cls.topology_api = cls.session.api.sdwan_feature_profiles.topology
+        cls.topology_profile_id = cls.topology_api.create_profile("TestProfile", "Description").id
+        # preconditions
+        cls.lanvpn_parcel_name = "VPN-1"
+        cls.service_api = cls.session.api.sdwan_feature_profiles.service
+        cls.service_profile_id = cls.service_api.create_profile("PreconditionServiceProfile", "Description").id
+        lanvpn_parcel = LanVpnParcel(
+            parcel_name=cls.lanvpn_parcel_name,
+            parcel_description="Test Vpn Parcel",
+            vpn_id=as_global(2),
+        )
+        cls.lanvpn_parcel_id = cls.service_api.create_parcel(cls.service_profile_id, lanvpn_parcel).id
 
     # TODO: need service parcel vpn api implemented to create referenced VPN-1 as precondition
     def test_mesh(self):
         mesh = MeshParcel(parcel_name="MeshParcel-1")
-        mesh.add_target_vpn("VPN-1")
+        mesh.add_target_vpn(self.lanvpn_parcel_name)
         mesh.add_site("SITE-1")
-        mesh_id = self.api.create_parcel(self.profile_id, mesh)
-        self.api.delete_parcel(self.profile_id, MeshParcel, mesh_id)
+        mesh_id = self.topology_api.create_parcel(self.topology_profile_id, mesh).id
+        self.topology_api.delete_parcel(self.topology_profile_id, MeshParcel, mesh_id)
 
     # TODO: need service parcel vpn api implemented to create referenced VPN-1 as precondition
     def test_hubspoke(self):
@@ -39,12 +55,13 @@ class TestTopologyFeatureProfileModels(unittest.TestCase):
         spoke = hubspoke.add_spoke(name="Spoke-1", spoke_sites=["SITE-1"])
         spoke.add_spoke_site("SITE-2")
         spoke.add_hub_site(["SITE-3"], preference=100891)
-        hubspoke.add_target_vpn("VPN-1")
+        hubspoke.add_target_vpn(self.lanvpn_parcel_name)
         hubspoke.add_selected_hub("HUB-1")
-        hubspoke_id = self.api.create_parcel(self.profile_id, hubspoke)
-        self.api.delete_parcel(self.profile_id, HubSpokeParcel, hubspoke_id)
+        hubspoke_id = self.topology_api.create_parcel(self.topology_profile_id, hubspoke).id
+        self.topology_api.delete_parcel(self.topology_profile_id, HubSpokeParcel, hubspoke_id)
 
     @classmethod
     def tearDownClass(cls) -> None:
-        cls.api.delete_profile(cls.profile_id)
+        cls.topology_api.delete_profile(cls.topology_profile_id)
+        cls.service_api.delete_profile(cls.service_profile_id)
         cls.session.close()
