76ded6ec29ece50bdb46e76be7290db69014e2e9
Jim Overholt <overholt@cisco.com>
2024-04-16 10:35:19 -0700

    monitoring

--- catalystwan/endpoints/endpoints_container.py
+++ catalystwan/endpoints/endpoints_container.py
@@ -85,6 +85,7 @@ from catalystwan.endpoints.configuration_settings import ConfigurationSettings
 from catalystwan.endpoints.misc import MiscellaneousEndpoints
 from catalystwan.endpoints.monitoring_device_details import MonitoringDeviceDetails
 from catalystwan.endpoints.monitoring_status import MonitoringStatus
+from catalystwan.endpoints.monitoring.server_info import ServerInfo
 from catalystwan.endpoints.real_time_monitoring.reboot_history import RealTimeMonitoringRebootHistory
 from catalystwan.endpoints.sdavc_cloud_connector import SDAVCCloudConnector
 from catalystwan.endpoints.tenant_backup_restore import TenantBackupRestore
@@ -198,6 +199,7 @@ class APIEndpointContainter:
         self.configuration_software_actions = ConfigurationSoftwareActions(session)
         self.configuration_disaster_recovery = ConfigurationDisasterRecovery(session)
         self.monitoring_device_details = MonitoringDeviceDetails(session)
+        self.monitoring_server_info = ServerInfo(session)
         self.monitoring_status = MonitoringStatus(session)
         self.sdavc_cloud_connector = SDAVCCloudConnector(session)
         self.tenant_backup_restore = TenantBackupRestore(session)
