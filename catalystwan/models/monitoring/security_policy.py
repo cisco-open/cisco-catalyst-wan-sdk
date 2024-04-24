2e9da0da36b6daf4c63948bc91af9b97e901cfaa
Robert Mooy <robertmooy@yahoo.com>
2024-04-24 08:31:18 -0700

    Update security_policy.py

--- catalystwan/models/monitoring/security_policy.py
+++ catalystwan/models/monitoring/security_policy.py
@@ -8,25 +8,24 @@ class DeviceListResponse(BaseModel):
 
     def get_amp_down(self) -> list:
         return self.data["amp_down"]
-        # amp_down: list = Field(..., alias="amp_down")
+
     def get_amp_up(self) -> list:
         return self.data["amp_up"]
-        # amp_up: list = Field(..., alias="amp_up")
+
     def get_ips_down(self) -> list:
         return self.data["ips_down"]
-        # ips_down: list = Field(..., alias="ips_down")
+
     def get_ips_up(self) -> list:
         return self.data["ips_up"]
-        # ips_up: list = Field(..., alias="ips_up")
+
     def get_urlf_down(self) -> list:
         return self.data["urlf_down"]
-        # urlf_down: list = Field(..., alias="urlf_down")
+
     def get_urlf_up(self) -> list:
         return self.data["urlf_up"]
-        # urlf_up: list = Field(..., alias="urlf_up")
+
     def get_zbfw_down(self) -> list:
         return self.data["zbfw_down"]
-        # zbfw_down: list = Field(..., serialization_alias="zbfw_down", validation_alias="zbfw_down")
+
     def get_zbfw_up(self) -> list:
         return self.data["zbfw_up"]
-        # zbfw_up: list = Field(..., serialization_alias="zbfw_up", validation_alias="zbfw_up")
