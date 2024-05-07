49b0911037016ca10302672629627657a653648f
renukavinay <renuka.channapatna@gmail.com>
2024-05-07 16:37:25 -0700

    unit tests added for url_monitoring

--- catalystwan/tests/test_url_monitoring.py
+++ catalystwan/tests/test_url_monitoring.py
@@ -1,46 +1,99 @@
 import unittest
 from unittest.mock import MagicMock, patch
+from catalystwan.typed_list import DataSequence
 
 from catalystwan.endpoints.url_monitoring import UrlMonitoring
-from catalystwan.models.url_monitoring import UrlMonitoringInfo #, UrlMonitoringConfig, DeleteUrlMonitorRequest
+from catalystwan.models.url_monitoring import UrlMonitoringInfo, UrlMonitoringConfig, DeleteUrlMonitorRequest
 
 
 class TestUrlMonitoring(unittest.TestCase):
-    def setUp(self) -> None:
-        self.mock_url_monitor_info_data = [{
+    @patch("catalystwan.session.ManagerSession")
+    def setUp(self, session_mock) -> None:
+        self.session = session_mock
+        self.session.api_version = None
+        self.session.session_type = None
+        self.session.password = "<>"
+        self.api = UrlMonitoring(self.session)
+        self.api._endpoints = MagicMock()
+
+        self.mock_url_monitor_info_data = {
             "url": "/client/server/ready",
-            "threshold": 20,
+            "threshold": 98,
             "alarm_raised": False
-        }]
+        }
+
         self.mock_url_monitor_config_add_data = {
             "url": "/client/server/ready",
             "threshold": 98
         }
+        self.mock_url_monitor_config_update_data = {
+            "url": "/client/server/ready",
+            "threshold": 90
+        }
+        self.mock_url_monitor_delete_url = DeleteUrlMonitorRequest(url="/client/server/ready")
 
 
     @patch("catalystwan.session.ManagerSession")
-    def test_get_url_monitor(self, mock_session):
+    def test_add_url_monitor(self, mock_session):
         # Arrange
-        # Create a MagicMock for the response that has a dataobj method
         mock_response = MagicMock()
-        # Set up the mock response's dataobj method to return a UrlMonitoringInfo
-        mock_response.dataobj.return_value = UrlMonitoringInfo(**self.mock_url_monitor_info_data)
+        mock_session.get_data.return_value = UrlMonitoringConfig(**self.mock_url_monitor_config_add_data)
+        # Mock the request method of the ManagerSession to return the mock response
+        mock_session_instance = mock_session.return_value
+        mock_session_instance.request.return_value = mock_response
+        mock_session.post.return_value = mock_response
+        url_monitor = UrlMonitoring(mock_session_instance)
+
+        # Act
+        response = url_monitor.add_url_monitor(self.mock_url_monitor_config_add_data)
+        # Assert
+        self.assertEqual(response, None)
+        # Ensure the request method was called
+        mock_session_instance.request.assert_called_once()
+
+
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.response.ManagerResponse")
+    def test_get_url_monitor(self, mock_session, mock_response):
+        # Arrange
+        mock_response = MagicMock()
+        mock_session_instance = mock_session.return_value
+        mock_session_instance.request.return_value = mock_response
+        url_monitor = UrlMonitoring(mock_session_instance)
+        observed_url_monitor = url_monitor.get_url_monitor()
+        #Assert
+        self.assertIsNotNone(observed_url_monitor)
+
+
+    @patch("catalystwan.session.ManagerSession")
+    def test_update_url_monitor(self, mock_session):
+        # Arrange
+        mock_response = MagicMock()
+        mock_session.get_data.return_value = UrlMonitoringConfig(**self.mock_url_monitor_config_update_data)
         # Mock the request method of the ManagerSession to return the mock response
         mock_session_instance = mock_session.return_value
         mock_session_instance.request.return_value = mock_response
         url_monitor = UrlMonitoring(mock_session_instance)
-
         # Act
-        response = url_monitor.get_url_monitor()
-        # response = self.client.get("/url/monitor")
-        # Additional assertions to test the response content
+        response = url_monitor.update_url_monitor(self.mock_url_monitor_config_update_data)
         # Assert
-        # self.assertIsInstance(response, UrlMonitoringInfo)
-        self.assertEqual(response[0].url, self.mock_server_info_data["url"])
-        self.assertEqual(response[0].threshold, self.mock_server_info_data["threshold"])
-        self.assertEqual(response[0].alarm_raised, self.mock_server_info_data["alarm_raised"])
-        self.assertEqual(response[0].url, "/client/server/ready")
-        self.assertEqual(response[0].threshold, 20)
-        self.assertEqual(response[0].alarm_raised, False)
+        self.assertEqual(response, None)
+        # Ensure the request method was called
+        mock_session_instance.request.assert_called_once()
+
+
+    @patch("catalystwan.session.ManagerSession")
+    def test_delete_url_monitor(self, mock_session):
+        # Arrange
+        mock_response = MagicMock()
+        mock_session.get_data.return_value = self.mock_url_monitor_delete_url
+        # Mock the request method of the ManagerSession to return the mock response
+        mock_session_instance = mock_session.return_value
+        mock_session_instance.request.return_value = mock_response
+        url_monitor = UrlMonitoring(mock_session_instance)
+        # Act
+        response = url_monitor.delete_url_monitor(self.mock_url_monitor_delete_url)
+        # Assert
+        self.assertEqual(response, None)
         # Ensure the request method was called
         mock_session_instance.request.assert_called_once()
