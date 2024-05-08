eafab5cdbe25ec8c0c7e1cecb11a3926f1e3b8d8
Piotr Smialkowski <psmialko@cisco.com>
2024-05-08 13:16:09 +0100

    UX2.0 Add new methods to AAAParcel.
    Add method add_user() and set_server_auth_order() to AAAParcel.

--- catalystwan/integration_tests/feature_profile/sdwan/test_system.py
+++ catalystwan/integration_tests/feature_profile/sdwan/test_system.py
@@ -11,6 +11,66 @@ from catalystwan.models.configuration.feature_profile.sdwan.system import (
     SecurityParcel,
     SNMPParcel,
 )
+from catalystwan.models.configuration.feature_profile.sdwan.system.aaa import DEFAULT_USER_PRIVILEGE, AAAParcel
+
+
+class TestAAAParcel(TestFeatureProfileModels):
+    def setUp(self) -> None:
+        self.api = self.session.api.sdwan_feature_profiles.system
+        self.profile_id = self.api.create_profile("TestProfileService", "Description").id
+        self.config_id = self.session.api.config_group.create("TestConfigGroup", "Descr", "sdwan", [self.profile_id]).id
+
+    def test_simple_create(self):
+        # Arrange
+        parcel = AAAParcel.new("aaa-name", ["local"])
+        parcel.add_user("admin", "admin")
+        parcel_id = self.api.create_parcel(self.profile_id, parcel).id
+        parcel = self.api.get_parcel(self.profile_id, AAAParcel, parcel_id).payload
+
+        assert parcel.parcel_name == "aaa-name"
+        assert parcel.server_auth_order.value == ["local"]
+        assert len(parcel.user) == 1
+        assert parcel.user[0].name.value == "admin"
+        assert parcel.user[0].privilege.value == DEFAULT_USER_PRIVILEGE
+
+    def test_adding_user_to_existing_parcel(self):
+        # Arrange
+        parcel = AAAParcel.new("aaa-test", ["local"])
+        parcel.add_user("admin", "admin")
+        parcel_id = self.api.create_parcel(self.profile_id, parcel).id
+
+        parcel = self.api.get_parcel(self.profile_id, AAAParcel, parcel_id).payload
+
+        assert len(parcel.user) == 1
+
+        # Act
+        parcel.add_user("user1", "pass1")
+
+        self.api.update_parcel(self.profile_id, parcel, parcel_id)
+        parcel = self.api.get_parcel(self.profile_id, AAAParcel, parcel_id).payload
+
+        assert len(parcel.user) == 2
+
+    def test_set_server_auth_order_to_exitsing_parcel(self):
+        # Arrange
+        parcel = AAAParcel.new("aaa-test", ["local"])
+        parcel.add_user("admin", "admin")
+        parcel_id = self.api.create_parcel(self.profile_id, parcel).id
+
+        parcel = self.api.get_parcel(self.profile_id, AAAParcel, parcel_id).payload
+
+        # Act
+        parcel.set_server_auth_order(["local", "test"])
+
+        self.api.update_parcel(self.profile_id, parcel, parcel_id)
+        parcel = self.api.get_parcel(self.profile_id, AAAParcel, parcel_id).payload
+
+        # Assert
+        assert len(parcel.server_auth_order.value) == 2
+
+    def tearDown(self) -> None:
+        self.session.api.config_group.delete(self.config_id)
+        self.api.delete_profile(self.profile_id)
 
 
 class TestSystemFeatureProfileModels(TestFeatureProfileModels):
@@ -19,6 +79,11 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         super().setUpClass()
         cls.api = cls.session.api.sdwan_feature_profiles.system
         cls.profile_uuid = cls.api.create_profile("TestProfileService", "Description").id
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.api.delete_profile(cls.profile_uuid)
+        super().tearDownClass()
 
     def test_when_default_values_banner_parcel_expect_successful_post(self):
         # Arrange
@@ -31,7 +96,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, banner_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, BannerParcel, parcel_id)
 
     def test_when_fully_specified_banner_parcel_expect_successful_post(self):
         # Arrange
@@ -46,7 +111,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, banner_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, BannerParcel, parcel_id)
 
     def test_when_default_values_logging_parcel_expect_successful_post(self):
         # Arrange
@@ -59,7 +124,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, logging_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, LoggingParcel, parcel_id)
 
     def test_when_fully_specified_logging_parcel_expect_successful_post(self):
         # Arrange
@@ -105,7 +170,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, logging_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, LoggingParcel, parcel_id)
 
     def test_when_default_values_bfd_parcel_expect_successful_post(self):
         # Arrange
@@ -118,7 +183,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, bfd_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, BFDParcel, parcel_id)
 
     def test_when_fully_specified_bfd_parcel_expect_successful_post(self):
         # Arrange
@@ -138,7 +203,7 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         # Assert
         assert parcel_id
 
-        self.api.delete_parcel(self.profile_uuid, bfd_parcel, parcel_id)
+        self.api.delete_parcel(self.profile_uuid, BFDParcel, parcel_id)
 
     def test_when_default_values_basic_parcel_expect_successful_post(self):
         # Arrange
@@ -216,8 +281,3 @@ class TestSystemFeatureProfileModels(TestFeatureProfileModels):
         parcel_id = self.api.create_parcel(self.profile_uuid, omp_parcel).id
         # Assert
         assert parcel_id
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.api.delete_profile(cls.profile_uuid)
-        super().tearDownClass()
