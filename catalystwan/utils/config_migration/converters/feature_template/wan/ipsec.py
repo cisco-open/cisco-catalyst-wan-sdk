3b7c064a662b5440b6b445d284934982cfd5a239
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-05-14 00:31:13 +0200

    Finish work

--- catalystwan/utils/config_migration/converters/feature_template/wan/ipsec.py
+++ catalystwan/utils/config_migration/converters/feature_template/wan/ipsec.py
@@ -1,9 +1,10 @@
 import pprint
 from copy import deepcopy
-from typing import Union
+from ipaddress import IPv6Address
+from typing import Optional, Union
 
 from catalystwan.api.configuration_groups.parcel import Default, Global, as_default, as_global
-from catalystwan.models.common import IpsecCiphersuite
+from catalystwan.models.common import IkeGroup, IpsecCiphersuite
 from catalystwan.models.configuration.feature_profile.common import AddressWithMask, TunnelApplication
 from catalystwan.models.configuration.feature_profile.sdwan.transport.wan.interface.ipsec import (
     InterfaceIpsecParcel,
@@ -24,8 +25,8 @@ class WanInterfaceIpsecTemplateConverter:
         if_description = data.get("description")
         application = self.parse_application(data)
         address = self.parse_address(data)
-        tunnel_source = data.get("tunnel_source")
-        tunnel_destination = data.get("tunnel_destination")
+        tunnel_source = self.parse_tunnel_source(data)
+        tunnel_destination = self.parse_tunnel_destination(data)
         mtu = data.get("mtu")
         tcp_mss_adjust = data.get("tcp_mss_adjust")
         dpd_interval = data.get("dead_peer_detection", {}).get("dpd_interval")
@@ -34,18 +35,18 @@ class WanInterfaceIpsecTemplateConverter:
         ike_version = ike.get("ike_version")
         ike_rekey_interval = ike.get("ike_rekey_interval")
         ike_ciphersuite = ike.get("ike_ciphersuite")
-        ike_group = ike.get("ike_group")
+        ike_group = self.parse_ike_group(data)
         authentication_type = ike.get("authentication_type", {})
         pre_shared_key = authentication_type.get("pre_shared_key", {})
         ike_local_id = pre_shared_key.get("ike_local_id")
         ike_remote_id = pre_shared_key.get("ike_remote_id")
-        pre_shared_secret = pre_shared_key.get("pre_shared_secret")
+        pre_shared_secret = self.parse_pre_shared_secret(data)
         ipsec_rekey_interval = data.get("ipsec", {}).get("ipsec_rekey_interval")
         ipsec_replay_window = data.get("ipsec", {}).get("ipsec_replay_window")
         ipsec_ciphersuite = self.parse_ipsec_cyphersuite(data)
         perfect_forward_secrecy = self.parse_perfect_forward_secrecy(data)
         clear_dont_fragment = data.get("clear_dont_fragment")
-        tracker = data.get("tracker")
+        tracker = self.parse_tracker(data)
         tunnel_source_interface = data.get("tunnel_source_interface")
 
         payload = create_dict_without_none(
@@ -83,13 +84,33 @@ class WanInterfaceIpsecTemplateConverter:
     def parse_address(self, data: dict) -> AddressWithMask:
         address = data.get("ip", {}).get("address", {})
         if not address:
-            raise CatalystwanConverterCantConvertException("Address is required")
+            raise CatalystwanConverterCantConvertException("Interface address is required")
         return AddressWithMask(
             address=as_global(address.value.network.network_address),
             mask=as_global(str(address.value.netmask)),
         )
 
-    def parse_pre_pre_shared_secret(self, data: dict) -> Global[str]:
+    def parse_tunnel_source(self, data: dict) -> AddressWithMask:
+        tunnel_source = data.get("tunnel_source")
+        if not tunnel_source:
+            raise CatalystwanConverterCantConvertException("Tunnel source is required")
+        return AddressWithMask(
+            address=tunnel_source,
+            mask=as_global("0.0.0.0"),  # UI sends this
+        )
+
+    def parse_tunnel_destination(self, data: dict) -> AddressWithMask:
+        tunnel_destination = data.get("tunnel_destination")
+        if not tunnel_destination:
+            raise CatalystwanConverterCantConvertException("Tunnel destination is required")
+        if isinstance(tunnel_destination.value, IPv6Address):
+            raise CatalystwanConverterCantConvertException("IPv6 is not supported for tunnel destination")
+        return AddressWithMask(
+            address=tunnel_destination,
+            mask=as_global("0.0.0.0"),  # UI sends this
+        )
+
+    def parse_pre_shared_secret(self, data: dict) -> Global[str]:
         pre_shared_secret = (
             data.get("ike", {}).get("authentication_type", {}).get("pre_shared_key", {}).get("pre_shared_secret")
         )
@@ -114,3 +135,12 @@ class WanInterfaceIpsecTemplateConverter:
         if perfect_forward_secrecy := data.get("ipsec", {}).get("perfect_forward_secrecy"):
             return as_global(perfect_forward_secrecy.value, PerfectForwardSecrecy)
         return as_default("group-16", PerfectForwardSecrecy)
+
+    def parse_ike_group(self, data: dict) -> Optional[Global[str]]:
+        if ike_group := data.get("ike", {}).get("ike_group"):
+            return as_global(ike_group.value, IkeGroup)
+        return None
+
+    def parse_tracker(self, data: dict) -> Default[None]:
+        # TODO: Implement tracker
+        return Default[None](value=None)
