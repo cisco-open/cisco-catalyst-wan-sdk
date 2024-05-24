03b9842a27d95a85c3718a336b58abf00070fcf1
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-05-24 18:59:57 +0200

    Fix report.py

--- catalystwan/api/builders/feature_profiles/report.py
+++ catalystwan/api/builders/feature_profiles/report.py
@@ -1,6 +1,6 @@
+# Copyright 2023 Cisco Systems, Inc. and its affiliates
 import functools
-import logging
-from typing import Any, List, Optional, Tuple, Union, cast
+from typing import Any, List, Optional, Tuple, Union
 from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict, Field
@@ -97,9 +97,8 @@ def handle_build_report_for_failed_subparcel(
     parent_failed_to_create_message = (
         f"Parent parcel: {parent.parcel_name} failed to create. This subparcel is dependent on it."
     )
-    logger.error(parent_failed_to_create_message)
     build_report.add_failed_parcel(
         parcel_name=subparcel.parcel_name,
-        parcel_type=cast(ParcelType, subparcel._get_parcel_type()),
+        parcel_type=subparcel._get_parcel_type(),
         error_info=parent_failed_to_create_message,
     )
