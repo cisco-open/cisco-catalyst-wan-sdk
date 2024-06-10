69375c37263e9c45cfd33542db76e4e7de527b87
Radoslaw Krawczyk <rkrawczy@cisco.com>
2024-06-10 12:26:54 +0200

    Parcels sort and references updating refactor

--- catalystwan/utils/config_migration/creators/policy_object_pusher.py
+++ catalystwan/utils/config_migration/creators/policy_object_pusher.py
@@ -6,7 +6,7 @@ from uuid import UUID
 
 from catalystwan.api.feature_profile_api import PolicyObjectFeatureProfileAPI
 from catalystwan.exceptions import ManagerHTTPError
-from catalystwan.models.configuration.config_migration import TransformedParcel, UX2Config, UX2ConfigPushResult
+from catalystwan.models.configuration.config_migration import UX2Config, UX2ConfigPushResult
 from catalystwan.models.configuration.feature_profile.common import FeatureProfileCreationPayload, RefIdItem
 from catalystwan.models.configuration.feature_profile.parcel import AnyParcel, Parcel, list_types
 from catalystwan.models.configuration.feature_profile.sdwan.policy_object import (
@@ -25,23 +25,11 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class ReferencesUpdater(ABC):
-    transformed_parcel: TransformedParcel
+    parcel: AnyPolicyObjectParcel
     pushed_objects_map: Dict[UUID, UUID]
-
-    def get_parcel(self) -> AnyParcel:
-        return self.transformed_parcel.parcel
-
-    @property
-    @abstractmethod
-    def push_order(self) -> int:
-        pass
 
     @abstractmethod
     def update_references(self):
-        pass
-
-    @abstractmethod
-    def are_references_update_required(self) -> bool:
         pass
 
     def get_v2_uuid_of_transferred_v1_policy(self, v1_uuid: UUID) -> UUID:
@@ -52,138 +40,77 @@ class ReferencesUpdater(ABC):
             f"Cannot find transferred policy object based on v1 API id: {v1_uuid}"
         )
 
-    def __lt__(self, other):
-        return self.push_order > other.push_order
-
-
-class DummyReferencesUpdater(ReferencesUpdater):
-    def update_references(self):
-        return
-
-    def are_references_update_required(self) -> bool:
-        return False
-
-    @property
-    def push_order(self) -> int:
-        return 0
-
 
 class UrlFilteringReferencesUpdater(ReferencesUpdater):
-    def are_references_update_required(self) -> bool:
-        url_filtering = cast(UrlFilteringParcel, self.transformed_parcel.parcel)
-        return bool(url_filtering.url_blocked_list or url_filtering.url_allowed_list)
-
-    @property
-    def push_order(self) -> int:
-        return 1
-
     def update_references(self):
-        if not self.are_references_update_required():
-            return
-
-        if allowed_list := self.transformed_parcel.parcel.url_allowed_list:
+        if allowed_list := self.parcel.url_allowed_list:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(allowed_list.ref_id.value))
-            self.transformed_parcel.parcel.url_allowed_list = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.url_allowed_list = RefIdItem.from_uuid(v2_uuid)
 
-        if blocked_list := self.transformed_parcel.parcel.url_blocked_list:
+        if blocked_list := self.parcel.url_blocked_list:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(blocked_list.ref_id.value))
-            self.transformed_parcel.parcel.url_blocked_list = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.url_blocked_list = RefIdItem.from_uuid(v2_uuid)
 
 
-class SslProfileReferencesUpdater(ReferencesUpdater):  # merge with url filtering?
-    def are_references_update_required(self) -> bool:
-        ssl_profile = cast(SslDecryptionProfileParcel, self.transformed_parcel.parcel)
-        return bool(ssl_profile.url_allowed_list or ssl_profile.url_blocked_list)
-
-    @property
-    def push_order(self) -> int:
-        return 1
-
+class SslProfileReferencesUpdater(ReferencesUpdater):
     def update_references(self):
-        if not self.are_references_update_required():
-            return
-
-        if allowed_list := self.transformed_parcel.parcel.url_allowed_list:
+        if allowed_list := self.parcel.url_allowed_list:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(allowed_list.ref_id.value))
-            self.transformed_parcel.parcel.url_allowed_list = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.url_allowed_list = RefIdItem.from_uuid(v2_uuid)
 
-        if blocked_list := self.transformed_parcel.parcel.url_blocked_list:
+        if blocked_list := self.parcel.url_blocked_list:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(blocked_list.ref_id.value))
-            self.transformed_parcel.parcel.url_blocked_list = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.url_blocked_list = RefIdItem.from_uuid(v2_uuid)
 
 
 class IntrusionPreventionReferencesUpdater(ReferencesUpdater):
-    def are_references_update_required(self) -> bool:
-        intrusion_prevention_profile = cast(IntrusionPreventionParcel, self.transformed_parcel.parcel)
-        return bool(intrusion_prevention_profile.signature_allowed_list)
-
-    @property
-    def push_order(self) -> int:
-        return 1
-
     def update_references(self):
-        if not self.are_references_update_required():
-            return
-
-        allowed_list = self.transformed_parcel.parcel.signature_allowed_list
-        v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(allowed_list.ref_id.value))
-        self.transformed_parcel.parcel.signature_allowed_list = RefIdItem.from_uuid(v2_uuid)
+        if allowed_list := self.parcel.signature_allowed_list:
+            v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(allowed_list.ref_id.value))
+            self.parcel.signature_allowed_list = RefIdItem.from_uuid(v2_uuid)
 
 
 class AdvancedInspectionProfileReferencesUpdater(ReferencesUpdater):
-    def are_references_update_required(self) -> bool:
-        aip_parcel = cast(AdvancedInspectionProfileParcel, self.transformed_parcel.parcel)
-        return any(
-            [
-                aip_parcel.intrusion_prevention,
-                aip_parcel.url_filtering,
-                aip_parcel.advanced_malware_protection,
-                aip_parcel.ssl_decryption_profile,
-            ]
-        )
-
-    @property
-    def push_order(self) -> int:
-        return 2
-
     def update_references(self):
-        if not self.are_references_update_required():
-            return
-
-        if advanced_malware_protection := self.transformed_parcel.parcel.advanced_malware_protection:
+        if advanced_malware_protection := self.parcel.advanced_malware_protection:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(advanced_malware_protection.ref_id.value))
-            self.transformed_parcel.parcel.advanced_malware_protection = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.advanced_malware_protection = RefIdItem.from_uuid(v2_uuid)
 
-        if intrusion_prevention := self.transformed_parcel.parcel.intrusion_prevention:
+        if intrusion_prevention := self.parcel.intrusion_prevention:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(intrusion_prevention.ref_id.value))
-            self.transformed_parcel.parcel.intrusion_prevention = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.intrusion_prevention = RefIdItem.from_uuid(v2_uuid)
 
-        if ssl_decryption_profile := self.transformed_parcel.parcel.ssl_decryption_profile:
+        if ssl_decryption_profile := self.parcel.ssl_decryption_profile:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(ssl_decryption_profile.ref_id.value))
-            self.transformed_parcel.parcel.ssl_decryption_profile = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.ssl_decryption_profile = RefIdItem.from_uuid(v2_uuid)
 
-        if url_filtering := self.transformed_parcel.parcel.url_filtering:
+        if url_filtering := self.parcel.url_filtering:
             v2_uuid = self.get_v2_uuid_of_transferred_v1_policy(UUID(url_filtering.ref_id.value))
-            self.transformed_parcel.parcel.url_filtering = RefIdItem.from_uuid(v2_uuid)
+            self.parcel.url_filtering = RefIdItem.from_uuid(v2_uuid)
 
 
-REFERENCES_UPDATER_MAPPING: Mapping[str, Callable] = {
-    "UrlFilteringParcel": UrlFilteringReferencesUpdater,
-    "SslDecryptionProfileParcel": SslProfileReferencesUpdater,
-    "IntrusionPreventionParcel": IntrusionPreventionReferencesUpdater,
-    "AdvancedInspectionProfileParcel": AdvancedInspectionProfileReferencesUpdater,
+REFERENCES_UPDATER_MAPPING: Mapping[type, Type[ReferencesUpdater]] = {
+    UrlFilteringParcel: UrlFilteringReferencesUpdater,
+    SslDecryptionProfileParcel: SslProfileReferencesUpdater,
+    IntrusionPreventionParcel: IntrusionPreventionReferencesUpdater,
+    AdvancedInspectionProfileParcel: AdvancedInspectionProfileReferencesUpdater,
+}
+
+POLICY_OBJECTS_PUSH_ORDER: Mapping[Type[AnyParcel], int] = {
+    UrlFilteringParcel: 1,
+    SslDecryptionProfileParcel: 1,
+    IntrusionPreventionParcel: 1,
+    AdvancedInspectionProfileParcel: 2,
 }
 
 
-def create_references_updater(
-    transformed_parcel: TransformedParcel,
-    pushed_objects_map: Dict[UUID, UUID],
-) -> ReferencesUpdater:
-    type_name = type(transformed_parcel.parcel).__name__
-    return REFERENCES_UPDATER_MAPPING.get(type_name, DummyReferencesUpdater)(
-        transformed_parcel=transformed_parcel,
-        pushed_objects_map=pushed_objects_map,
-    )
+def get_parcel_ordering_value(parcel: Type[AnyParcel]) -> int:
+    return POLICY_OBJECTS_PUSH_ORDER.get(parcel, 0)
+
+
+def update_parcels_references(parcel: AnyPolicyObjectParcel, pushed_objects_map: Dict[UUID, UUID]) -> None:
+    if ref_updater := REFERENCES_UPDATER_MAPPING.get(type(parcel)):
+        ref_updater(parcel, pushed_objects_map=pushed_objects_map).update_references()
 
 
 class PolicyObjectPusher:
@@ -214,17 +141,18 @@ class PolicyObjectPusher:
         # will hold system created parcels id by type and name when detected
         system_created_parcels: Dict[Type[AnyPolicyObjectParcel], Dict[str, UUID]] = {}
 
-        transormed_policies_ref_updaters = [
-            create_references_updater(transformed_parcel, self._pushed_objects_map)
-            for transformed_parcel in self._ux2_config.profile_parcels
-            if type(transformed_parcel.parcel) in list_types(AnyPolicyObjectParcel)
-        ]
+        transformed_parcels = sorted(
+            [
+                transformed_parcel
+                for transformed_parcel in self._ux2_config.profile_parcels
+                if type(transformed_parcel.parcel) in list_types(AnyPolicyObjectParcel)
+            ],
+            key=lambda x: get_parcel_ordering_value(type(x.parcel)),  # sorter
+        )
 
-        transormed_policies_ref_updaters.sort(reverse=True)
-
-        for i, transormed_policy_ref_updater in enumerate(transormed_policies_ref_updaters):
-            parcel = cast_(transormed_policy_ref_updater.get_parcel())
-            header = transormed_policy_ref_updater.transformed_parcel.header
+        for i, transformed_parcel in enumerate(transformed_parcels):
+            parcel = cast_(transformed_parcel.parcel)
+            header = transformed_parcel.header
             parcel_type = type(parcel)
 
             # update existing system created parcels
@@ -245,16 +173,17 @@ class PolicyObjectPusher:
                 continue
 
             try:
-                transormed_policy_ref_updater.update_references()
+                update_parcels_references(parcel, self._pushed_objects_map)
+
                 parcel_id = self._policy_object_api.create(profile_id=default_profile_id, payload=parcel).id
                 profile_rollback.add_parcel(parcel.type_, parcel_id)
                 self._push_result.report.groups_of_interest.add_created(parcel.parcel_name, parcel_id)
-                self._pushed_objects_map[transormed_policy_ref_updater.transformed_parcel.header.origin] = parcel_id
+                self._pushed_objects_map[transformed_parcel.header.origin] = parcel_id
 
                 self._progress(
                     f"Creating Policy Object Parcel: {parcel.parcel_name}",
                     i + 1,
-                    len(transormed_policies_ref_updaters),
+                    len(transformed_parcels),
                 )
             except ManagerHTTPError as e:
                 logger.error(f"Error occured during config group creation: {e.info}")
