bc0eaf835dce431a66e35a29be34b67a32f6c09f
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-06-11 15:53:16 +0200

    update license plate

--- catalystwan/models/common.py
+++ catalystwan/models/common.py
@@ -1,5 +1,4 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
-# Copyright 2023 Cisco Systems, Inc. and its affiliates
 
 from dataclasses import InitVar, dataclass, field
 from typing import Any, Dict, Iterator, List, Literal, Mapping, Optional, Sequence, Set, Tuple, Union
