6ae1fefa2788aab5e87c07d4a45d5e995926f7ca
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-06-28 14:23:33 +0200

    Minor changes

--- catalystwan/utils/config_migration/converters/feature_template/base.py
+++ catalystwan/utils/config_migration/converters/feature_template/base.py
@@ -1,4 +1,5 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
+from abc import ABC, abstractmethod
 from typing import Optional, Tuple
 
 from catalystwan.models.configuration.config_migration import ConvertResult
@@ -6,27 +7,29 @@ from catalystwan.models.configuration.feature_profile.parcel import AnyParcel
 from catalystwan.utils.config_migration.converters.exceptions import CatalystwanConverterCantConvertException
 
 
-class FTConverter:
-    """Feature Template Converter base class."""
+class FTConverter(ABC):
+    """Feature Template Converter abstract base class."""
 
     supported_template_types: Tuple = tuple()
 
-    def __init__(self) -> None:
-        if not self.supported_template_types:
-            raise ValueError("supported_template_types must be defined in the subclass")
+    def __init__(self):
         self._convert_result = ConvertResult[AnyParcel]()
 
+    @abstractmethod
     def create_parcel(self, name: str, description: str, template_values: dict) -> Optional[AnyParcel]:
         """This function can modify status and info attributes of the conversion process."""
-        raise NotImplementedError("create_parcel method must be implemented")
+        ...
 
     def convert(self, name: str, description: str, template_values: dict) -> ConvertResult[AnyParcel]:
-        """Converts the template values into a parcel."""
-        parcel: Optional[AnyParcel] = None
+        if not self.supported_template_types:
+            self._convert_result.update_status(
+                "unsupported", "supported_template_types must be defined in the subclass"
+            )
+            return self._convert_result
+
         try:
             parcel = self.create_parcel(name, description, template_values)
+            self._convert_result.output = parcel
         except CatalystwanConverterCantConvertException as e:
             self._convert_result.update_status("failed", str(e))
-
-        self._convert_result.output = parcel
         return self._convert_result
