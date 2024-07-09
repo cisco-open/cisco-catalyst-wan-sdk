1f87aa0816fcaf211872f99d94346ecbc49aa358
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-07-09 13:51:05 +0200

    Fix license header

--- catalystwan/tests/config_migration/test_data/__init__.py
+++ catalystwan/tests/config_migration/test_data/__init__.py
@@ -1,5 +1,4 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
-# Copyright 2023 Cisco Systems, Inc. and its affiliates
 from typing import List
 
 from .feature_templates.dhcp import dhcp_server
