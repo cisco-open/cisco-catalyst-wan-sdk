d9e8d572407b64b632e48a7ee72019ca2ce90f9f
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-07-10 13:29:34 +0200

    Constant value convernted to variable value for integrity-type from Cisco Secuirty feature template #58

--- catalystwan/models/configuration/feature_profile/sdwan/system/security.py
+++ catalystwan/models/configuration/feature_profile/sdwan/system/security.py
@@ -1,10 +1,11 @@
+# Copyright 2024 Cisco Systems, Inc. and its affiliates
 from __future__ import annotations
 
 from typing import List, Literal, Optional, Union
 
 from pydantic import AliasPath, BaseModel, ConfigDict, Field
 
-from catalystwan.api.configuration_groups.parcel import Default, Global, Variable, _ParcelBase, as_default, as_variable
+from catalystwan.api.configuration_groups.parcel import Default, Global, Variable, _ParcelBase, as_default
 
 IntegrityType = Literal["esp", "ip-udp-esp", "none", "ip-udp-esp-no-id"]
 ReplayWindow = Literal["64", "128", "256", "512", "1024", "2048", "4096", "8192"]
@@ -44,7 +45,7 @@ class OneOfendChoice3(BaseModel):
         extra="forbid",
         populate_by_name=True,
     )
-    exact: Global[float] = Field(..., description="Configure Key lifetime end time")
+    exact: Global[int] = Field(..., description="Configure Key lifetime end time")
 
 
 class SendLifetime(BaseModel):
@@ -59,7 +60,7 @@ class SendLifetime(BaseModel):
     local: Optional[Union[Variable, Global[bool], Default[bool]]] = Field(
         default=None, description="Configure Send lifetime Local"
     )
-    start_epoch: Global[float] = Field(
+    start_epoch: Global[int] = Field(
         ...,
         serialization_alias="startEpoch",
         validation_alias="startEpoch",
@@ -82,7 +83,7 @@ class AcceptLifetime(BaseModel):
     local: Optional[Union[Variable, Global[bool], Default[bool]]] = Field(
         default=None, description="Configure Send lifetime Local"
     )
-    start_epoch: Global[float] = Field(
+    start_epoch: Global[int] = Field(
         ...,
         serialization_alias="startEpoch",
         validation_alias="startEpoch",
@@ -162,7 +163,6 @@ class SecurityParcel(_ParcelBase):
         description="Extended Anti-Replay Window",
     )
     integrity_type: Union[Global[List[IntegrityType]], Variable] = Field(
-        default=as_variable("{{security_auth_type_inte}}"),
         validation_alias=AliasPath("data", "integrityType"),
         description="Set the authentication type for DTLS connections",
     )
