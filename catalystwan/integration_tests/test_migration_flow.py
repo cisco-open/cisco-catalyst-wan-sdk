a10b8a378a81f590e8ab7cbe899a15088419d582
Jakub Krajewski <jakubpkrajewski@gmail.com>
2024-07-15 12:27:19 +0200

    Run all tests from migration flow after successful ci setup

--- catalystwan/integration_tests/test_migration_flow.py
+++ catalystwan/integration_tests/test_migration_flow.py
@@ -1,6 +1,5 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
 
-import unittest
 
 from catalystwan.integration_tests.base import TestCaseBase
 from catalystwan.utils.config_migration.runner import ConfigMigrationRunner
@@ -13,24 +12,20 @@ class TestMigrationFlow(TestCaseBase):
     def setUpClass(cls) -> None:
         super().setUpClass()
         cls.runner = ConfigMigrationRunner.collect_push_and_rollback(cls.session)
-        cls.runner.dump_schemas()
-        # cls.runner.run()
+        cls.runner.run()
 
-    @unittest.skip("Testing purpose only")
     def test_collect_artefact(self):
         # Arrange
         ux1_config = self.runner.load_collected_config()
         # Act, Assert
         assert ux1_config
 
-    @unittest.skip("Testing purpose only")
     def test_transform_artefact(self):
         # Arrange
         transform_result = self.runner.load_transform_result()
         # Act, Assert
         assert transform_result.failed_items == list()
 
-    @unittest.skip("Testing purpose only")
     def test_push_artefact(self):
         # Arrange
         push_result = self.runner.load_push_result()
